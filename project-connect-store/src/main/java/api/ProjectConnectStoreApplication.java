c874c4c4a7b6cd66ed5734595c899c976cf786f7
luanrichardsz <luanrchardof@gmail.com>
2025-09-05 11:12:17 -0300

    chore: configuração inicial do projeto com Spring Boot

---
+++ project-connect-store/src/main/java/api/ProjectConnectStoreApplication.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectConnectStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectConnectStoreApplication.class, args);
+	}
+
+}
---
+++ project-connect-store/src/test/java/com/project_store/api/ProjectConnectStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_store.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectConnectStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
